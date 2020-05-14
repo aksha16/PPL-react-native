2014e2711556a583ce034401c2c5768b2b60dd93
Aksha <aksha.ali@daffodilsw.com>
2020-05-14 13:45:01 +0530

    existing repo

--- components/timeline.js
+++ components/timeline.js
@@ -8,19 +8,45 @@ import {
   TouchableHighlight,
   ActivityIndicator,
   StyleSheet,
-  ScrollView
+  ScrollView,
 } from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Home!</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Settings!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 const Timeline = () => {
   return (
+    <>
     <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
       <ScrollView>
         <Text>Hi, I'm TimeLine. What's up????</Text>
       </ScrollView>
-      <View>
-        <Text>footer</Text>
-      </View>
     </View>
+    <NavigationContainer independent={true}>
+      <Tab.Navigator>
+        <Tab.Screen name='Home' component={HomeScreen}/>
+        <Tab.Screen name='Settings' component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
+    </>
   );
 };
 
