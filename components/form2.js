dd1731e913598c47a38b0194702c8dd250c09ebb
Aksha <aksha.ali@daffodilsw.com>
2020-06-01 17:44:40 +0530

    existing repo

--- components/form2.js
+++ components/form2.js
@@ -12,28 +12,42 @@ import {useForm, Controller} from 'react-hook-form';
 import styles from '../styles';
 import {ScrollView} from 'react-native-gesture-handler';
 
-const validate = (email) => {
-    const emailRe = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-      if (emailRe.test(email) === false) {
-          error.email.msg = "Email address is invalid"
-      }
-}
+// const validate = value => {
+//   const errors = {};
+//   const emailRe = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+//   if (emailRe.test(email) === false) {
+//     error.email = 'Email address is invalid';
+//   }
+//   return errors;
+// };
 
 export default function App() {
-    
-  const {register, setValue, handleSubmit, errors, control} = useForm(validate);
+  const {register, setValue, handleSubmit, errors, control} = useForm();
   const onSubmit = data => Alert.alert('Form Data', JSON.stringify(data));
 
   useEffect(() => {
     register({name: 'firstname'}, {required: true});
     register({name: 'lastname'});
     register({name: 'username'}, {required: true});
-    register({name: 'email'}, {required:true});
-    register({name: 'password'}, {required:true});
+    register(
+      {name: 'email'},
+      {
+        required: true,
+        pattern: /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i,
+      },
+    );
+    register(
+      {name: 'password'},
+      {
+        required: true,
+        pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/,
+      },
+    );
   }, [register]);
 
   return (
     <ScrollView>
+      {console.log('hahahah Errors', errors)}
       <View style={styles.container}>
         <View style={styles.login_sec}>
           <Image style={styleIn.image} source={require('../images/logo.png')} />
@@ -53,6 +67,15 @@ export default function App() {
           )}
 
           <Text style={styleIn.text}>Email</Text>
+          {errors.email ? (
+            errors.email.type === 'pattern' && (
+              <Text style={styleIn.error}>
+                Invalid Email, try somthing like abc@xyz.com
+              </Text>
+            )
+          ) : (
+            <></>
+          )}
           <Controller
             as={TextInput}
             control={control}
@@ -61,10 +84,25 @@ export default function App() {
             rules={{required: true}}
             defaultValue=""
             placeholder="email"
-            
           />
-          {errors.email && <Text style={styleIn.error}>This is required.</Text>}
+          {errors.email ? (
+            errors.email.type === 'required' && (
+              <Text style={styleIn.error}>This is required.</Text>
+            )
+          ) : (
+            <></>
+          )}
 
+          {errors.password ? (
+            errors.password.type === 'pattern' && (
+              <Text style={styleIn.error}>
+                Invalid password. Password must of atleast 9 characters which
+                include a special character, numbers and alphabets.
+              </Text>
+            )
+          ) : (
+            <></>
+          )}
           <Text style={styleIn.text}>Password</Text>
           <Controller
             as={TextInput}
@@ -76,8 +114,12 @@ export default function App() {
             defaultValue=""
             placeholder="password"
           />
-          {errors.password && (
-            <Text style={styleIn.error}>This is required.</Text>
+          {errors.password ? (
+            errors.password.type === 'required' && (
+              <Text style={styleIn.error}>This is required.</Text>
+            )
+          ) : (
+            <></>
           )}
 
           <Text style={styleIn.text}>Firstname</Text>
@@ -90,6 +132,7 @@ export default function App() {
             defaultValue=""
             placeholder="firstname"
           />
+
           {errors.firstname && (
             <Text style={styleIn.error}>This is required.</Text>
           )}
